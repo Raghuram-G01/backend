7272bbca75c78b415837930e075a2edf0d81b909
Raghuram <raghuram.g2024cse@sece.ac.in>
2025-12-16 14:02:49 +0530

    modified some of the files

--- db-connect/db.js
+++ db-connect/db.js
@@ -1,19 +1,13 @@
 const mongoose = require("mongoose");
+
 exports.connect = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/LMS-Backend");
+    await mongoose.connect(
+      "mongodb+srv://ak7859437:lUdNj7EzHx6ItZfx@clusterlove.uvn8cgb.mongodb.net/LMS?retryWrites=true&w=majority&appName=ClusterLove"
+    );
     console.log("LMS Db is connected");
   } catch (e) {
     console.log("Error in connecting to the db");
+    console.error(e.message);
   }
 };
-
-//get
-
-//post
-
-//put
-
-//delete
-
-//

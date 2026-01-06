938f5959ca1a532870c30bc659283e8fb94830a8
Sreesairam V <sairam2381@users.noreply.github.com>
2026-01-06 07:38:11 +0530

    Adding new .env

--- db-connect/db.js
+++ db-connect/db.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 exports.connect = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://ak7859437:lUdNj7EzHx6ItZfx@clusterlove.uvn8cgb.mongodb.net/LMS?retryWrites=true&w=majority&appName=ClusterLove"
+      "mongodb+srv://devopsstudy883_db_user:cpJp77PzjtoRLmci@cluster0.yxr54kq.mongodb.net/"
     );
-  
-  // try {
-  //   const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/LMS-Backend';
-  //   const conn = await mongoose.connect(mongoURI);
-  //   console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    // try {
+    //   const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/LMS-Backend';
+    //   const conn = await mongoose.connect(mongoURI);
+    //   console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (e) {
     console.log("Error in connecting to the db");
     console.error(e.message);
